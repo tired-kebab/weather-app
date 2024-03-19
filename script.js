0d76cf85d46daaead0a24c81a3ae460d7db62ad3
tired-kebab <malik2015naeem@gmail.com>
2024-03-19 07:50:42 +0530

    refactored the code for better interface

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const inputBox = document.getElementById("input-box");
 const submitBtn = document.getElementById("submit-btn");
+const weatherInfo = document.querySelector(".weather");
+const cityInfo = document.querySelector(".city");
+const tempInfo = document.querySelector(".temp");
+const humidityInfo = document.querySelector(".humidity");
+const windInfo = document.querySelector(".wind");
 
 let city;
 
 async function checkWeather() {
   const response = await fetch(
-    "https://api.api-ninjas.com/v1/weather?city=" + city,
+    `https://api.api-ninjas.com/v1/weather?city=${city}`,
     { headers: { "X-API-Key": "wYTTkwhrLA8ntlwiIt9rlg==A1qERtFVhXY9UZ1H" } }
   );
+
+  if (city) {
+    weatherInfo.classList.remove("hidden");
+  }
+
   const data = await response.json();
   console.log(data);
-  document.querySelector(".city").innerHTML = inputBox.value;
-  document.querySelector(".temp").innerHTML = data.temp + "°C";
-  document.querySelector(".humidity").innerHTML = data.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind_speed + " km/h";
+  cityInfo.innerHTML = inputBox.value;
+  tempInfo.innerHTML = `${data.temp}°C`;
+  humidityInfo.innerHTML = `${data.humidity}%`;
+  windInfo.innerHTML = `${data.wind_speed} km/h`;
+  inputBox.value = "";
 }
 
 submitBtn.addEventListener("click", () => {
   city = inputBox.value;
-  //   inputBox.value = "";
   checkWeather();
 });
